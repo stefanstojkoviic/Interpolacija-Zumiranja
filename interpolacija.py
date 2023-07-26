1329112ab16f816d38ffb34b9b1213b801af5df3
Stefan Stojkovic <sstefan.kv748@gmail.com>
2023-07-26 14:02:28 +0200

    Izmenjen nacin zumiranja. Treba dodati bilinearnu interpolaciju.

--- interpolacija.py
+++ interpolacija.py
@@ -4,13 +4,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #Import slike i konvertovanje je u RGB(U open-cv biblioteci default je BGR)
-img = cv.imread('zaba.jpg')
+img = cv.imread('katedrala.jpg')
 img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 
 #2x2 plotovi za prikaz
 fig, ax = plt.subplots(2, 2, figsize=(12, 6))
-
-scale = 2.0 #uvelicavac
+#Naslov
+fig.suptitle('Interpolacija Zumiranja')
+#uvelicavac
+scale = 2.0 
 
 #Velicina slike i broj kanala
 rows, cols, kanal = img.shape
@@ -23,6 +25,13 @@ new_cols = int(cols * scale)
 #Kreirana matrica sa povecanim dimenzijama i sa pocetnim vrednostima 0 (crnim pixelima)
 zoomed = np.zeros((new_rows, new_cols, kanal), dtype=np.uint8)
 
+
+#Originalna slika
+ax[0,0].set_title('Originalna slika')
+ax[0,0].imshow(img)
+ax[0,0].axis('off')
+
+
 #Zumiranje slike bez interpolacije
 """
 Svaki piksel u originalnoj slici se kopira na odgovarajuce mesto u zumiranoj slici.
@@ -33,41 +42,56 @@ for i in range(rows):
     for j in range(cols):
         zoomed[int(i * scale), int(j * scale)] = img[i, j]
 
-#Export slike:
-cv.imwrite('zoomed.jpg', cv.cvtColor(zoomed, cv.COLOR_RGB2BGR))
+zoomed2=np.zeros((rows,cols,3),dtype=np.uint8)
 
-#Originalna slika
-ax[0,0].set_title('Originalna slika')
-ax[0,0].imshow(img)
+for i in range(rows):
+    for j in range(cols):
+        zoomed2[i,j]=zoomed[i,j]
 
-#Zumirana slika bez interpolacije
-ax[0,1].set_title('#Zumirana slika bez interpolacije')
-ax[0,1].set_xlim([1000,1600])
-ax[0,1].set_ylim([500,200])
-ax[0,1].imshow(zoomed)
+ax[0,1].set_title('Zumirana slika bez interpolacije')
+ax[0,1].imshow(zoomed2)
+ax[0,1].axis('off')
 
+#Export slike bez interpolacije:
+# cv.imwrite('zoomed.jpg', cv.cvtColor(zoomed2, cv.COLOR_RGB2BGR))
+
+#Kopiranje matrice zoomed za drugu interpolaciju
+bilinear=zoomed2.copy()
 
 #Interpolacija najblizeg komsije
-
 """
 Ovde racunamo odgovarajucu vrednost piksela u originalnoj slici koristeci interpolaciju najblizeg komsije.
 Za svaki piksel u zumiranoj slici, pronalazimo najblizi odgovarajuci piksel u originalnoj slici.
 Ovo postizemo deljenjem koordinata piksela u zumiranoj slici promenjivom 'scale',
 a zatim uzimamo celobrojni deo da bismo dobili najblizi piksel u originalnoj slici.
 """
-for i in range(new_rows):
-    for j in range(new_cols):
-        zoomed[i,j] = img[int(i/scale), int(j/scale)]
+for i in range(rows):
+    for j in range(cols):
+        zoomed2[i,j] = img[int(i/scale), int(j/scale)]
 
 #Zumirana slika sa interpolaciom najblizeg suseda
 ax[1,0].set_title('Slika sa interpolacijom najblizeg suseda')
-ax[1,0].set_xlim([1000,1600])
-ax[1,0].set_ylim([500,200])
-ax[1,0].imshow(zoomed)
+ax[1,0].imshow(zoomed2)
+ax[1,0].axis('off')
+
+
+
+
+
+# Bilinearna interpolacija:
+"""
+Bilinearna interpolacija uzima u obzir
+više okolnih piksela kako bi izračunao vrednost piksela između postojećih tačaka.
+Ovde se za svaki piksel u zumiranoj slici računa odgovarajuća vrednost koristeći bilinearnu interpolaciju.
+"""
+
+#dodati bilinearnu interpolaciju
 
 
 #Zumirana slika sa bilinearnom interpolaciom
-ax[1,1].set_title('Slika sa drugom interpolacijiom')
-# ax[1,1].imshow()
+ax[1,1].set_title('Slika sa bilinearnom interpolacijiom')
+ax[1,1].imshow(bilinear)
+ax[1,1].axis('off')
+
 
 plt.show()
